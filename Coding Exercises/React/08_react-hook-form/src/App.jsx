f41c150baf622039a7cf3f4078940c4e8a7ae6c2
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-01-21 20:28:19 +0530

    Basic Form Creation completed

--- Coding Exercises/React/08_react-hook-form/src/App.jsx
+++ Coding Exercises/React/08_react-hook-form/src/App.jsx
@@ -18,17 +18,35 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>First Name: </label>
-          <input {...register('firstName')} />
+          <input {...register('firstName', 
+            { 
+              required: true, 
+              maxLength: 20, 
+              minLength: {value: 2, message: "First name should be at least 2 characters long"} 
+              })} />
+              {errors.firstName && <p>{errors.firstName.message}</p>}
         </div>
         <br />
         <div>
           <label>Middle Name: </label>
-          <input {...register('middleName')} />
+          <input {...register('middleName', 
+          { 
+            required: true, 
+            maxLength: 20, 
+            minLength: {value: 2, message: "Middle name should be at least 2 characters long"} 
+          })} />
+          {errors.middleName && <p>{errors.middleName.message}</p>}
         </div>
         <br />
         <div>
           <label>Last Name: </label>
-          <input {...register('lastName')} />
+          <input {...register('lastName', 
+            {
+             required: true, 
+             maxLength: 20, 
+             minLength: {value: 2, message: "Last name should be at least 2 characters long"} 
+            })} />
+            {errors.lastName && <p>{errors.lastName.message}</p>}
         </div>
         <br />
         <input type="submit" />

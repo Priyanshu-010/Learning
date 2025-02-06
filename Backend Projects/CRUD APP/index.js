cf6bcc83bcaf92a5065a7ca996ac3018e8318f50
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-06 22:36:33 +0530

    Db connected

--- Backend Projects/CRUD APP/index.js
+++ Backend Projects/CRUD APP/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
 const port = 3000
+
+mongoose.connect("mongodb+srv://priyanshurai2772:PqOGAcfAGG7bmHpf@cluster0.nh8yb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+  console.log("Connected to Database")
+}).catch((err) => {
+  console.log(err)
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World")

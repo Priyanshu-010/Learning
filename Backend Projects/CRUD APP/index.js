724ebdbc8be7dc664217e51d232db07df36d7ac3
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-08 17:05:21 +0530

    create functionality

--- Backend Projects/CRUD APP/index.js
+++ Backend Projects/CRUD APP/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Product from './models/product.model.js';
 
 const app = express();
 const port = 3000
+
+app.use(express.json())
+
 
 mongoose.connect("mongodb+srv://priyanshurai2772:PqOGAcfAGG7bmHpf@cluster0.nh8yb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0").then(() => {
   console.log("Connected to Database")
@@ -12,6 +16,24 @@ mongoose.connect("mongodb+srv://priyanshurai2772:PqOGAcfAGG7bmHpf@cluster0.nh8yb
 
 app.get("/", (req, res) => {
   res.send("Hello World")
+})
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(201).json(product)
+  } catch (error) {
+    res.status(500).json({messaage: error.message})
+  }
+})
+
+app.get("/api/products", async(req, res) =>{
+  try {
+    const product = await Product.find({})
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({messaage: error.message})
+  }
 })
 
 app.listen(port, ()=>{

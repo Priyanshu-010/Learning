9f54fcf0cf199a9ba995c2f56e3cc9c8f14f03d1
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-08 18:17:11 +0530

    CRUD APP Completed

--- Backend Projects/CRUD APP/index.js
+++ Backend Projects/CRUD APP/index.js
@@ -21,50 +21,6 @@ app.get("/", (req, res) => {
   res.send("Hello World")
 })
 
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body)
-    res.status(201).json(product)
-  } catch (error) {
-    res.status(500).json({messaage: error.message})
-  }
-})
-
-
-app.put("/api/products/:id", async(req, res) =>{
-  try {
-    const {id} = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body)
-    
-    if(!product){
-      return res.status(404).json({messaage: "Product not found"})
-    }
-
-    const updatedProduct = await Product.findById(id)
-    res.status(200).json(updatedProduct)
-
-
-  } catch (error) {
-    res.status(500).json({messaage: error.message})
-  }
-})
-
-app.delete("/api/products/:id", async(req, res) =>{
-  try {
-    const {id} =req.params;
-    const product = await Product.findByIdAndDelete(id);
-
-    if(!product){
-      return res.status(404).json({messaage: "Product not found"})
-    }
-
-    res.status(200).json({messaage: "Product deleted"})
-  } catch (error) {
-    res.status(500).json({messaage: error.message})
-  }
-})
-
-
 app.listen(port, ()=>{
   console.log(`Server is running on port ${port}`)
 })
--- Backend Projects/CRUD APP/routes/product.route.js
+++ Backend Projects/CRUD APP/routes/product.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import Product from '../models/product.model.js';
-import { getAllProducts, getSingleProducts } from '../controller/product.controller.js';``
+import { createProduct, deleteProduct, getAllProducts, getSingleProducts, updateProduct } from '../controller/product.controller.js';
 
 const router = express.Router();
 router.get("/", getAllProducts)
 router.get("/:id", getSingleProducts)
+router.post("/", createProduct)
+router.put("/:id", updateProduct)
+router.delete("/:id", deleteProduct)
 
 export default router

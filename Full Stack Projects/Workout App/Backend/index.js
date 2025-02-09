acac4d33a1d3f06bae688283deb9373ffbf46833
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-09 19:49:18 +0530

    Middlewares

--- Full Stack Projects/Workout App/Backend/index.js
+++ Full Stack Projects/Workout App/Backend/index.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+app.use((req, res, next)=>{
+  console.log(req.path, req.method)
+  next()
+})
+
 app.get("/", (req, res) => {  
   res.send("Hello World")
 })

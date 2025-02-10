f16400858f4917915fc5dfa8560fa71ca345ee5d
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-10 18:01:11 +0530

    Controllers made

--- Full Stack Projects/Workout App/Backend/controllers/workouts.controller.js
+++ Full Stack Projects/Workout App/Backend/controllers/workouts.controller.js
@@ -32,7 +32,11 @@ export const updateWorkout = async (req, res) => {
   try {
     const {id} = req.params
     const workout = await Workouts.findByIdAndUpdate(id, req.body)
-    res.status(200).json(workout)
+    if(!workout){
+      return res.status(404).json({message: "Workout not found"})
+    }
+    const updatedWorkout = await Workouts.findById(id)
+    res.status(200).json(updatedWorkout, {message: "Workout updated"})
   } catch (error) {
     res.status(500).json({message: error.message})
   }
@@ -42,7 +46,9 @@ export const deleteWorkout = async (req, res) => {
   try {
     const {id} = req.params
     const workout = await Workouts.findByIdAndDelete(id)
-    res.status(200).json(workout)
+
+    
+    res.status(200).json(workout, {message: "Workout deleted"})
   } catch (error) {
     res.status(500).json({message: error.message})
   }

1d9711ce1b2bad24c7a7cf504ca699ffec9a1be2
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-15 15:54:22 +0530

    UserAuth setup

--- Full Stack Projects/Workout App/Backend/controllers/user.controller.js
+++ Full Stack Projects/Workout App/Backend/controllers/user.controller.js
@@ -2,4 +2,6 @@ export const loginUser = async (req, res) => {
   res.json({message: "Login User"})
 } 
 
-export const signupUser = async (req, res) => {} 
+export const signupUser = async (req, res) => {
+  res.json({message: "Signup User"})
+} 

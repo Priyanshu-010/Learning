1034214f9b6b5ebfc60ff574094f8674db01b860
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-18 22:29:55 +0530

    Signup Function half made in user model

--- Full Stack Projects/Workout App/Backend/controllers/user.controller.js
+++ Full Stack Projects/Workout App/Backend/controllers/user.controller.js
@@ -1,7 +1,20 @@
+import User from "../models/user.model";
+
 export const loginUser = async (req, res) => {
   res.json({message: "Login User"})
 } 
 
 export const signupUser = async (req, res) => {
+
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    
+    res.status(200).json({ email, user})
+  } catch (error) {
+   res.status(400).json({error: error.message})
+  }
+
   res.json({message: "Signup User"})
 } 
--- Full Stack Projects/Workout App/Backend/models/user.model.js
+++ Full Stack Projects/Workout App/Backend/models/user.model.js
@@ -6,11 +6,26 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  password: { 
+  password: {
     type: String,
     required: true
   }
 })
+
+userSchema.statics.signup = async function (email, password) {
+
+  const exists = await this.findOne({ email });
+
+  if(exists){
+    throw Error('Email already in use')
+  }
+
+  const salt = await bcyrpt.salt(10);
+  const hash = await bcyrpt.hash(password, salt);
+  const user = await this.create({email, password: hash});
+
+  return user
+}
 
 const User = mongoose.model("User", userSchema)
 

efca1a730a34b2a6e8668a9b12641401686f77e5
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-21 20:37:31 +0530

    Get Single and all todos and create a todo functionality added

---
+++ Backend Projects/CRUD APP BY ME/controller/todo.controller.js
@@ -0,0 +1,30 @@
+import Todo from "../model/todo.model.js"
+
+
+export const getAllTodo = async (req, res) =>{
+  try {
+    const todo = await Todo.find({});
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+}
+
+export const getSingleTodo = async (req, res) =>{
+  try {
+    const {id} = req.params;
+    const todo = await Todo.findById(id);
+    res.status(200).json(todo)
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
+
+export const createTodo = async (req,res) =>{
+  try {
+    const todo = await Todo.create(req.body);
+    res.status(200).json(todo)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+}
--- Backend Projects/CRUD APP BY ME/routes/todo.route.js
+++ Backend Projects/CRUD APP BY ME/routes/todo.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { createTodo, getAllTodo, getSingleTodo } from '../controller/todo.controller.js';
 
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-  res.send("Hello World")
-})
+router.get("/", getAllTodo)
+router.get("/:id", getSingleTodo)
+router.post("/", createTodo);
 
 export default router

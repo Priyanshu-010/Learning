84ad265d5a77dc304ed60745fcc8785ed5d6ee58
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-21 16:05:55 +0530

    Sign up user completed

--- Full Stack Projects/Workout App/Backend/controllers/user.controller.js
+++ Full Stack Projects/Workout App/Backend/controllers/user.controller.js
@@ -1,8 +1,12 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id)=>{
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 export const loginUser = async (req, res) => {
-  
-  res.json({message: "Login User"})
+  res.send("Login User")
 } 
 
 export const signupUser = async (req, res) => {
@@ -11,11 +15,11 @@ export const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
+
+    const token = createToken(user._id);
     
-    res.status(200).json(user)
+    res.status(200).json({email, token})
   } catch (error) {
    res.status(400).json({error: error.message})
   }
-
-  res.json({message: "Signup User"})
 } 
--- Full Stack Projects/Workout App/Backend/models/user.model.js
+++ Full Stack Projects/Workout App/Backend/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,7 +14,23 @@ const userSchema = new mongoose.Schema({
   }
 })
 
+// Signup
+
 userSchema.statics.signup = async function(email, password) {
+
+  if(!email || !password){
+    throw Error('All fields must be filled')
+  }
+
+  if(!validator.isEmail(email)){
+    throw Error('Email is not valid')
+  }
+  
+  // Commented this password validator for development purposes
+
+  // if(!validator.isStrongPassword(password)){
+  //   throw Error('Password is not strong enough')
+  // }
 
   const exists = await this.findOne({ email });
 
@@ -25,6 +42,28 @@ userSchema.statics.signup = async function(email, password) {
   const hash = await bcrypt.hash(password.toString(), salt);
 
   const user = await this.create({email, password: hash});
+
+  return user
+}
+
+// Login
+
+userSchema.statics.login = async function(email, password) {
+  if(!email || !password){
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({email});
+
+  if(!user){
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if(!match){
+    throw Error('Incorrect password')
+  }
 
   return user
 }

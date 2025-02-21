84f3ed0b0be48bea16f01379561c9615af695283
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-02-21 20:46:15 +0530

    Crud app by me completed

--- Backend Projects/CRUD APP BY ME/controller/todo.controller.js
+++ Backend Projects/CRUD APP BY ME/controller/todo.controller.js
@@ -33,6 +33,11 @@ export const updateTodo = async (req, res) =>{
   try {
     const {id} = req.params;
     const todo = await Todo.findByIdAndUpdate(id, req.body);
+
+    if(!todo){
+      res.status(404).json({message: "Todo not found"})
+    }
+
     const updatedTodo = await Todo.findById(id);
     res.status(200).json(updatedTodo)
   } catch (error) {
@@ -44,6 +49,11 @@ export const deleteTodo = async (req, res) =>{
   try {
     const {id} = req.params;
     const todo = await Todo.findByIdAndDelete(id);
+
+    if(!todo){
+      res.status(404).json({message: "Todo not found"})
+    }
+    
     res.status(200).json({message: "Todo deleted"})
   } catch (error) {
     res.status(500).json({message: error.message})

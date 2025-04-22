7fc95e9f3029da3130d1281bdb61bc278206e44e
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-22 22:23:25 +0530

    List API

--- Full Stack Projects/Todo App/Backend/src/index.js
+++ Full Stack Projects/Todo App/Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {connectDb}  from "./connection/db.js";
 import authRouter from "./routes/auth.route.js"
+import listRouter from "./routes/list.route.js"
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,7 @@ app.get("/", (req, res) =>{
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/api/auth", authRouter)
+app.use("/api/list", listRouter)
 
 app.listen(port, ()=>{
   console.log(`Server is running on port ${port}`)

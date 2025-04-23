05311e16ca212e770882af93c717fd74ce2d47f3
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-23 15:54:38 +0530

    Crud operations functionality applied in backend

---
+++ Full Stack Projects/Todo App/Backend/src/contorllers/list.controller.js
@@ -0,0 +1,50 @@
+import User from '../models/user.model.js'
+import List from '../models/list.model.js'
+import { json } from 'express';
+
+export const createList = async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = new List({ title, body, user: existingUser });
+      await list.save();
+      res.status(201).json(list);
+      existingUser.list.push(list);
+      existingUser.save();
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const updateList = async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if(!existingUser){
+      return res.status(404).json({messaage: "Product not found"})
+    }
+
+    if (existingUser) {
+      await List.findByIdAndUpdate(req.params.id, { title, body });
+      
+      const updatedList = await List.findById(req.params.id);
+      res.status(200).json(updatedList);
+    }
+  } catch (error) {
+    console.log(error, json({message: "Something went wrong"}))
+  }
+}
+export const deleteList = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+      const list = await List.findByIdAndDelete(req.params.id);
+      res.status(200).json({message: "List deleted"})
+    }
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+}
--- Full Stack Projects/Todo App/Backend/src/routes/list.route.js
+++ Full Stack Projects/Todo App/Backend/src/routes/list.route.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { createList, deleteList, updateList } from "../contorllers/list.controller.js";
 
 const router = express.Router();
 
-router.get("/")
+router.post("/addTask", createList)
+router.put("/updateTask/:id", updateList)
+router.delete("/deleteTask/:id", deleteList)
 
 export default router

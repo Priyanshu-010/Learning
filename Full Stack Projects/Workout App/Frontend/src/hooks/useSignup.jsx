3031ccdac659ca194c6eaa03370bb56246de2c93
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-24 16:13:18 +0530

    Workoutapp

--- Full Stack Projects/Workout App/Frontend/src/hooks/useSignup.jsx
+++ Full Stack Projects/Workout App/Frontend/src/hooks/useSignup.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import useAuthContext from "./useAuthContext";
 
 import React from 'react'
@@ -6,6 +6,20 @@ import React from 'react'
 const useSignup = () => {
   const [error, setError] =useState(null)
   const [isLoading, setIsLoading] =useState(null)
+
+  const signup = async (email, password) => {
+    setIsLoading(true)
+    setError(null)
+
+    const respone = await fetch("http://localhost:3000/api/user/signup", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email, password})
+      })
+      const data = await respone.json()
+  }
   return (
     <div>useSignup</div>
   )

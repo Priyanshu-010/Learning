dc89cfe9ae16a6de8c69d2291096ff4889e0cda8
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-29 18:19:53 +0530

    Database connected

--- Full Stack Projects/Todo/backend/index.js
+++ Full Stack Projects/Todo/backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
 import todoRouter from "./routes/todo.js";
+import { connectDB } from "./utils/connect.js";
 
 
 const app = express();
@@ -16,4 +17,5 @@ app.use("/api/todo", todoRouter)
 
 app.listen(process.env.PORT, ()=> {
   console.log("Server is running on port 3000")
+  connectDB()
 })

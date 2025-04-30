be5c7460228ee946011db7ddac33c910ced26b5e
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-30 15:08:01 +0530

    Global errror handler

--- Full Stack Projects/Todo/backend/index.js
+++ Full Stack Projects/Todo/backend/index.js
@@ -15,6 +15,12 @@ app.get("/", (req, res) => {
 app.use("/api/user", authRouter)
 app.use("/api/todo", todoRouter)
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({message})
+})
+
 app.listen(process.env.PORT, ()=> {
   console.log("Server is running on port 3000")
   connectDB()

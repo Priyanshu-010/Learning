39160ba14825491c2ae5485119091d4f698abd50
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-30 15:38:39 +0530

    Backend Done without Auth

--- Full Stack Projects/Todo/backend/models/todo.model.js
+++ Full Stack Projects/Todo/backend/models/todo.model.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-  userId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: [true, "User Id is required"]
-  },
+  // userId:{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "user",
+  //   required: [true, "User Id is required"]
+  // },
   title:{
     type: String,
     required: true

7cf66e944a96960810884933c7640b2c8cf8f0e6
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-04-30 15:21:55 +0530

    Register

--- Full Stack Projects/Todo/backend/controllers/auth.js
+++ Full Stack Projects/Todo/backend/controllers/auth.js
@@ -1,11 +1,18 @@
-export const login = (req, res) => {
+import { createError } from "../utils/error.js";
+
+export const register = async (req, res, next) => {
+  const data = req.body;
+  console.log(data)
+  if(!data.email || !data.password){
+    return next(createError(400, "Email and Password are required")) 
+  }
+  res.send("Register Page")
+}
+
+export const login = async (req, res) => {
   res.send("Login Page")
 }
 
-export const register = (req, res) => {
-  res.send("register Page")
-}
-
-export const logout = (req, res) => {
+export const logout = async (req, res) => {
   res.send("logout Page")
 }
--- Full Stack Projects/Todo/backend/index.js
+++ Full Stack Projects/Todo/backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
 import todoRouter from "./routes/todo.js";
 import { connectDB } from "./utils/connect.js";
-
+import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
@@ -11,6 +11,9 @@ dotenv.config();
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use("/api/user", authRouter)
 app.use("/api/todo", todoRouter)

44ee2d7380bfc0d6a8f1c380434c9b28a55db6f2
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-03 12:24:52 +0530

    Typescript

---
+++ TypeScript/L1/app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = "5";
+var n2 = 10;
+console.log(add(n1, n2));
---
+++ TypeScript/L1/app.ts
@@ -0,0 +1,8 @@
+function add(n1, n2){
+  return n1 + n2
+}
+
+const n1 = "5";
+const n2 = 10;
+
+console.log(add(n1,n2));

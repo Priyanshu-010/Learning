3afd6b72bd2f02e1a9dfa7dd40db60367a5792c4
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-05 16:51:26 +0530

    comments

--- TypeScript/L2/app.ts
+++ TypeScript/L2/app.ts
@@ -1,10 +1,10 @@
 class Abcd{
   public _name: string;
   constructor(_name: string, public age: number){}
-  get name(){
+  get name(){  //This is not a function it is a property
     return this._name;
   }
-  set name(value:string){
+  set name(value:string){ //c1.name = "Rai" we have to use it like this,not like func
     this._name = value;
   }
 }

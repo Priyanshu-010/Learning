7d2962b9b1fe3d98f9e10954cedad52c6c204df2
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-06 16:16:25 +0530

    CRUD Functionality Completed

--- Full Stack Projects/Book Store/Backend/controllers/book.controller.js
+++ Full Stack Projects/Book Store/Backend/controllers/book.controller.js
@@ -30,3 +30,33 @@ export const getBook = async (req, res)=>{
     res.status(500).json({message: error.message})
   }
 }
+
+export const updateBook = async (req, res) =>{
+  try {
+    const {id} = req.params;
+    const book = await Book.findByIdAndUpdate(id, req.body);
+    if(!book){
+      return res.status(404).json({message: "Book not found"})
+    }
+    const updatedBook = await Book.findById(id);
+    res.status(200).json(updatedBook)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+}
+
+export const deleteBook = async(req, res) =>{
+  try {
+    const {id} = req.params;
+    const book = await Book.findByIdAndDelete(id);
+    if(!book){
+      return res.status(404).json({message: "Book not found"})
+    }
+
+    res.status(200).json({message: "Book deleted"})
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})  
+  }
+}
--- Full Stack Projects/Book Store/Backend/route/book.route.js
+++ Full Stack Projects/Book Store/Backend/route/book.route.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createBook, getAllBook, getBook } from "../controllers/book.controller.js"
+import { createBook, deleteBook, getAllBook, getBook, updateBook } from "../controllers/book.controller.js"
 
 const router = express.Router()
 
 router.get("/", getAllBook)
 router.post("/books", createBook)
 router.get("/:id", getBook)
+router.put("/:id", updateBook)
+router.delete("/:id", deleteBook)
 
 export default router

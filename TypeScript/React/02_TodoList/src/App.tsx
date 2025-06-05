febb7eb3a637c69e891aaf02b64bdfb360fde3ac
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-06-05 23:00:32 +0530

    Added the functionality of adding todos and showing them on UI

--- TypeScript/React/02_TodoList/src/App.tsx
+++ TypeScript/React/02_TodoList/src/App.tsx
@@ -2,16 +2,31 @@ import type React from 'react'
 import './App.css'
 import Input from './components/Input'
 import { useState } from 'react'
+import type { Todo } from './models/models'
 
 const App:React.FC = ()=> {
 
   const [todo, setTodo] = useState<string>('');
-  console.log(todo)
+  const [todos, setTodos] =useState<Todo[]>([]);
+  
+  const handleSubmit = (e: React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+      setTodo("")
+    }
+  }
+  console.log(todos)
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <Input todo={todo} setTodo={setTodo}/>
+      <Input todo={todo} setTodo={setTodo} handleSubmit={handleSubmit}/>
+      {todos.map((todo)=>(
+        <div className="todo" key={todo.id}>
+          <p>{todo.todo}</p>
+        </div>
+      ))}
     </div>
   )
 }
--- TypeScript/React/02_TodoList/src/components/Input.tsx
+++ TypeScript/React/02_TodoList/src/components/Input.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Style.css'
 
 interface Props {
   todo: string,
-  setTodo: React.Dispatch<React.SetStateAction<string>>
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+  handleSubmit: (e: React.FormEvent)=>void
 }
-const Input = ({todo, setTodo}: Props) => {
+// const Input = ({todo, setTodo, handleSubmit}: Props) => {
+
+// The above one and the below one are the same
+
+const Input: React.FC<Props> = ({todo, setTodo, handleSubmit}) => {
+  const inputRef = useRef<HTMLFormElement>(null)
   return (
-    <form className='input'>
-      <input 
+    <form className='input' onSubmit={(e)=>{
+      handleSubmit(e)
+      inputRef.current?.blur();
+    }}>
+      <input
+        ref={inputRef} 
         type="text"
         value={todo}
         onChange={(e)=> setTodo(e.target.value)} 

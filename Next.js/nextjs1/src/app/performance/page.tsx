7a0a72448fd243b37ac9813485882f45deac0941
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-06-23 18:01:18 +0530

    First Next.js App (Initialization)

---
+++ Next.js/nextjs1/src/app/performance/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PerformancePage = () => {
+  return (
+    <div>performancePage</div>
+  )
+}
+
+export default PerformancePage
---
+++ Next.js/nextjs1/src/app/reliability/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const reliabilityPage = () => {
+  return (
+    <div>reliabilityPage</div>
+  )
+}
+
+export default reliabilityPage

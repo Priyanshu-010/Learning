c0fee7ea3cee6211a6715fc064c4602381cfb62b
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-18 22:42:05 +0530

    Basic setup with Navbar and Shadcn UI

---
+++ Next.js/blog-app/src/app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Blog <span className="text-blue-500">Sphere</span>
+          </h1>
+        </Link>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link href="/" className="text-lg font-medium hover:text-blue-500">
+            Home
+          </Link>
+          <Link href="/dashboard" className="text-lg font-medium hover:text-blue-500">
+            Dashboard
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Button>Sign up</Button>
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

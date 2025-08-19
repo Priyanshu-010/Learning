a799a88f0d3dbabd5089a8e75579fc529839a17c
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-19 12:27:07 +0530

    Database and prisma orm implemented and model for blog created

--- Next.js/blog-app/src/app/page.tsx
+++ Next.js/blog-app/src/app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <div >
-      Home Page
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      </div>
     </div>
   );
 }

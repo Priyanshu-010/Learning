8f37e0fcc5e48861e6d18ab2b7d14b16cfbbc63e
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-23 19:32:24 +0530

    middleware

---
+++ Next.js/blog-app/middleware.ts
@@ -0,0 +1,11 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default function middleware() {
+} 
+
+export const config = {
+  matcher: [
+    // Run on everything but Next internals and static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ]
+}; 
